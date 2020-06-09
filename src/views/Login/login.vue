14c1ea85546d1e9f24b845532e8570268489fd47
LIXIANGQIAN <806169406@qq.com>
2020-06-09 21:22:07 +0800

    构建项目，配置初始化样式

---
+++ src/views/Login/login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div >
+        <div class="login">
+            <div class="login-wrap">
+              
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'login'
+}
+</script>
+<style lang="scss" scoped>
+    .login{
+        background-color: aqua;
+        width: 100%;
+        height: 100vh;
+    }
+</style>
